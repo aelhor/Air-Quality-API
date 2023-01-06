ebf39016c8aadf8c93558033773a099d74555650
ahmedel <aelhor90@mail.com>
2023-01-06 19:13:01 +0200

    added readme file

--- api/routes/mostPolParis.js
+++ api/routes/mostPolParis.js
@@ -6,11 +6,12 @@ const airQuality = require('../models/airQualityModel')
 router.get('/', async (req, res) => {
     try {
         const airQ = await airQuality.findOne().sort('aqius').limit(1).select('_id createdAt')
-        
 
-        res.send(
-            airQ
-        )
+
+        res.send({
+                _id: airQ._id, 
+                DateTime : airQ.createdAt
+            })
     } catch (error) {
         res.send(error)
     }
